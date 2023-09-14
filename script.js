003d1ae190bd957d19110f3be795499a0f18fa1a
umutesililiane <umutesililiane4@gmail.com>
2023-09-14 22:55:34 +0200

    adding pause and play functionality

---
+++ script.js
@@ -0,0 +1,22 @@
+
+let progress = document.getElementById("progress");
+let song = document.getElementById("song");
+let ctrlIcon = document.getElementById("ctrlIcon");
+
+song.onloadedmetadata = function(){
+    progress.max = song.duration;
+    progress.value = song.currentTime;
+}
+
+function playPause(){
+    if(ctrlIcon.classList.contains("fa-pause")){
+        song.pause();
+        ctrlIcon.classList.remove("fa-pause");
+        ctrlIcon.classList.add ("fa-play");
+    }
+    else{
+        song.play();
+        ctrlIcon.classList.add("fa-pause");
+        ctrlIcon.classList.remove ("fa-play");
+    }
+}
